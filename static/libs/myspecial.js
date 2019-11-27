7842435a55b7a98a90174b05889938db2ab22c7d
张立元 <zhangliyuan@okjiaoyu.cn>
2019-11-27 21:42:24 +0800

    jquery special

--- static/libs/myspecial.js
+++ static/libs/myspecial.js
@@ -1,74 +1,54 @@
 
 (function (_, jQuery) {
-    let i = 0;
-    function dbhandler() {
-        console.log('dblclick handler')
+    function dbhandler(e) {
+        console.log('dblclick handler', e.type, e.target)
     }
     jQuery.event.special['dblclick'] = {
         setup: function () {
-            console.log('dblclick count', i++)
             document.addEventListener('dblclick', dbhandler)
             return false;
         },
         teardown: function () {
             document.removeEventListener('dblclick', dbhandler)
             return false;
-        },
-        add: function () { },
-        remove: function () { }
+        }
     }
 
-    jQuery.event.special[''] = {
+ 
+    var elems = [];
+    jQuery.event.special['clickout'] = {
         setup: function () {
-            document.addEventListener('dblclick', dbhandler)
-            return false;
+            console.log('clickout setup')
+            // 依赖于事件的捕获机制
+            document.addEventListener('click', clickOutsideHandler, true)
         },
-        teardown: function () {
-            document.removeEventListener('dblclick', dbhandler)
-            return false;
-        },
-        add: function () { },
-        remove: function () { }
-    }
-
-    let c = 0;
-    let elems = _.elems = [];
-    jQuery.event.special['clickoutside'] = {
-        setup: function () {
-            c++;
-            if(c>1) return;
-            document.addEventListener('click', clickOutsideHandler)
-        },
-        teardown: function () {
-            document.removeEventListener('click', clickOutsideHandler)
-            return false;
-        },
-        add: function () {
-            if (elems.indexOf(this) === -1) {
-                elems.push(this)
+        teardown: function () { 
+            if( elems.length )return;
+            document.removeEventListener('click', clickOutsideHandler);
+            console.log('clickout teardown');
+         },
+        add: function (handleObj) {
+            var handler = handleObj.handler;
+            handleObj.handler = function (e) {
+                e.stopPropagation();
+                handler.call(this, e)
             }
+            elems.indexOf(this) === -1 && elems.push(this)
         },
         remove: function () {
-            console.log(arguments)
-            console.log(elems)
+            var elem = this;
             elems.forEach(function (v, i) {
-                if (v === this) {
-                    console.log(this, i)
-                    elems.splice(i, 1)
-                }
+                v === elem && elems.splice(i, 1)
             })
         }
     }
 
     function clickOutsideHandler(e) {
-        console.log(c)
         jQuery.each(elems, function (_, elem) {
             if (e.target !== elem && !elem.contains(e.target)) {
-                jQuery.event.simulate('clickoutside', elem, e)
+                jQuery.event.simulate('clickout', elem, jQuery.event.fix(e))
             }
         })
     }
-
-
 
 })(window, jQuery)
